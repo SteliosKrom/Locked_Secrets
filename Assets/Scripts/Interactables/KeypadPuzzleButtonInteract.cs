84781cc46583e8d17b7e9ca27837b28c4f7b98c9
SteliosKrom <stelios.yolokrom@gmail.com>
2026-02-22 20:54:44 +0200

    feat: keypad buttons animation

--- Assets/Scripts/Interactables/KeypadPuzzleButtonInteract.cs
+++ Assets/Scripts/Interactables/KeypadPuzzleButtonInteract.cs
@@ -1,14 +1,35 @@
+using System.Collections;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class KeypadPuzzleButtonInteract : MonoBehaviour, IInteractable
 {
+    private float buttonPressDelay = 0.75f;
+
     [SerializeField] private KeypadButtonRoles keypadButtonRoles;
 
+    private Animator keypadButtonAnimator;
+
     public KeypadButtonRoles KeypadPuzzleButtonRoles => keypadButtonRoles;
+
+    private void Awake()
+    {
+        keypadButtonAnimator = GetComponentInParent<Animator>();
+    }
 
     public void Interact()
     {
         PuzzleManager.Instance.OnKeypadPuzzleButtonInteracted(this);
         AudioManager.Instance.PlaySFX(AudioManager.Instance.KeypadButton.source, AudioManager.Instance.KeypadButton.clip);
+
+        keypadButtonAnimator.SetTrigger("Pressed");
+        StartCoroutine(DisableColliderTemporarily());
+    }
+
+    public IEnumerator DisableColliderTemporarily()
+    {
+        PuzzleManager.Instance.DisableKeypadButtonColliders();
+        yield return new WaitForSeconds(buttonPressDelay);
+        PuzzleManager.Instance.EnableKeypadPuzzleButtonColliders();
     }
 }
